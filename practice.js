d6ec532874c83030733b03b13a1bb5d68840e769
AHMAD-MOEED. <pilotmoeed878@gmail.com>
2024-11-01 13:59:31 +0500

    first commit

---
+++ practice.js
@@ -0,0 +1,2 @@
+var num = Math.round()
+console.log(num * 6 +1)
---
+++ script.js
@@ -0,0 +1,325 @@
+
+// let num1 = prompt("please inter number one") 
+//  let num2 =  prompt("please inter number Two") 
+// console.log( num1 - num2)
+//  console.log(num1 % num2 )
+//  console.log(num1 * num2)
+//  console.log(num1 / num2 )
+
+
+// let datenow= prompt("new date")
+// let dateold=prompt("old date")
+// console.log(datenow - dateold)
+
+// let  marks = prompt("enter marks")
+// if ( marks <= 100 && marks >= 90 ) {
+//     console.log("you are pass")
+// }
+// else{
+//     console.log("better luck next time")
+// }
+
+// let member=prompt("member")
+// let validcoupancode=prompt("valid coupan code")
+// let parchase=prompt("1000")
+
+// if ( member =="yes" &&  validcoupancode =="yes" && parchase >='1000') {
+//     console.log("you are eligible")
+// }
+// else{
+//     console.log("better luck next time")
+// }
+
+
+// let day=prompt("day")
+// let waether=prompt("weather")
+
+// if ( (day =="saturday" || day =="sunday") && waether=="sunny")
+//      {
+//     console.log("you are redy to go")
+// }
+// else{
+//     console.log("better luck next time")
+// }
+
+
+
+
+// let num = prompt("Enter a number:");
+
+// if (num % 2 === 0) {
+//     console.log(num + " is even");
+// }
+// else {
+//     console.log(num + " is odd");
+// }
+
+// let email=prompt("email bjbab")
+// let pass=prompt("pass")
+
+// if (email=="ahmadmoeed@gmail.com") {
+//     if ( pass=="12345") {
+//         console.log( "pass is correct")
+//     } 
+//     else{
+//         console.log("pass is incorrect")
+//     }
+
+//     console.log("email is correct")
+// }
+// else {
+//     console.log ( "both r correct")
+// }
+
+
+// let fruits=["apple" , "mango" , "banana",  "anar" , "waterm"]
+// fruits.shift()
+// console.log(fruits)
+
+
+
+// for ( let index = 10 ; index >= 0; index--){
+//     console.log( index + "x" + "2" + "=" + index * 2 )
+// }
+
+
+// let Array = ["one" ,"two","three", "four" , "five"]
+
+// for (let index=0 ; index< Array.length ; index++){
+//     console.log( Array[index])
+// }
+
+
+
+
+// let firstNames = ["ahmad ", "muhammad ", "faiq ", "zakir ", "hamad "];
+//  let lastNames = ["khan", "ali", "kahn", "ali" , "moeed"];
+//  let fullNames = [];
+//  for (var i = 0; i < firstNames.length; i++) {
+//  for (var j = 0; j < lastNames.length; j++) {
+//     if ( i=j){ 
+//  fullNames.push(firstNames[i] + lastNames[j]);
+//  }
+//  }
+//  }
+// console.log( fullNames)
+
+
+
+
+// let name = "ahmad"
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+
+
+
+// let startStaion = prompt("Enter Start Station ")
+// let endstation = prompt("enter end station")
+
+// if(startStaion == "university" && endstation == "sadder"){
+//     console.log("go to station b ");
+//     console.log("fare: 20pKR");
+// }
+// else if(startStaion == "sadder" && endstation == "hayatabad"){
+//     console.log("go to sattaion c");
+//     console.log("fare: 3oPKR");
+// }
+// else if (startStaion == "university" && endstation == "hayatabad"){
+//     console.log("go to sattaion c");
+//     console.log("fare: 40PKR");
+// } 
+// else {
+//     console.log("invalid route plese check your stations")
+// }
+
+
+
+
+// let month = prompt("Enter a month");
+// let charsInMonth = month.length;
+// if (charsInMonth > 3) {
+//     monthAbbrev = month.slice(0, 3);
+
+// }
+// console.log(monthAbbrev)
+
+// let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// let yellow= fruits.slice(2, 4);
+// console.log(yellow)
+
+
+// let fullName = "naseer"
+
+// let Name= fullName.toUpperCase()[0];
+
+// console.log(Name + "aseer" )
+
+
+
+
+// let abc = "khan"
+// let xyz = abc[0].toUpperCase() + abc.slice(1)
+// console.log (xyz)
+
+
+
+
+// let fullName = "ahmad";
+
+// console.log(fullName[0])
+// console.log(fullName[1])
+// console.log(fullName[2])
+// console.log(fullName[3])
+// console.log(fullName[4])
+// console.log(fullName[5])
+
+
+
+//  31 
+
+
+// let rightNow = new Date()
+// console.log(rightNow)
+// let theDay = rightNow.getDay()
+// console.log(theDay)
+
+
+// let dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+// let now = new Date()
+//  console.log(now)
+// let theDay = now.getDay() + now.getMonth()
+// console.log(theDay)
+// let nameOfToday = dayNames[theDay]
+// console.log(nameOfToday)
+// let num = Math.ran 
+// console.log(Math.round(99.95)
+
+// const now = new Date ()
+// console.log(now.getDay())
+
+// const now =  Month()
+// console.log(now.getMonth())
+
+
+// const d = new Date()
+// let month = d.getMonth()
+// console.log(month)
+
+// const month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+// const d = new Date()
+// let name = month(d.getMonth())
+// console.log(d)
+
+// function thisFunc ( num1 , num2){
+//     console.log(num1 - num2)
+
+
+
+// thisFunc(10 , 5)
+// thisFunc(15 , 5)
+// thisFunc(20 , 5)
+// thisFunc(25 , 5)
+// thisFunc(30 , 5)
+
+// function myName(){
+//     alert("Ahmad Moeed")
+// }
+// myName()
+
+
+// {
+//      number = 5
+//      console.log(number + 3)
+
+// }
+// console.log(number + 3)
+
+
+// switch (new Date().getMonth()) {
+//     case 0:
+//         month = "january"
+
+//         break;
+
+//     case 1:
+//         month = "september"
+
+//         break;
+
+//     case 2:
+//         month = "april"
+
+//         break;
+
+//     default:
+//         break;
+//         console.log(number + 3)
+
+
+// }
+
+
+// let grade = prompt("signal light")
+// switch (grade) {
+//     case "red":
+//         lightColor = "stop"
+//         break;
+//     case "yellow":
+//         lightColor = "redy guyz"
+//         break;
+//     case "green":
+//         lightColor = "u can go khow"
+//         break;
+//     default:
+//         lightColor = "stay safe"
+//         break;
+// }
+
+// console.log(lightColor)
+
+// let i =1
+// while (i<11) {
+//     console.log(i)
+//     i++
+    
+// }
+
+
+// function chnge() {
+//     console.log("chnge")
+    
+// }  
+
+ 
+// function px(e){
+//     console.log(e)
+// }
+
+let input =document.querySelector("input").value="new"
+console.log("input")
+
+.querySelector("input").value= "this  text"
+
+
+
+
+
+
+
+function red() {
+let pra = document.querySelector( "p").style.color="red"
+}
+
+function green() {
+    
+    let pra = document.querySelector( "p").text="tthi is new txt"
+    document.querySelector("p").innerHTML = "New text"
+
+    document.querySelector("p").style = "color: yellow; font-size: 50px"
+   
+    }
+    
+
+    function blue() {
+        let pra = document.querySelector( "p").style.color="blue"
+        }
